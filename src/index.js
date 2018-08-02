e1ed6ea7a8c0731a174b30cc3bc6d877294b8711
urielscola <urielsgc@gmail.com>
2018-08-02 00:25:08 -0300

    package.json update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-const server = require('./config/server.js');
-require('./config/database.js');
-require('./config/routes.js')(server);
+const app = require('./app');
+const PORT = process.env.PORT || 3003;
+
+app.listen(PORT, function() {
+	console.log(`Server is running on port ${process.env.PORT || PORT}.`);
+});
--- src/server.js
+++
@@ -1,6 +0,0 @@
-const app = require('./app');
-const PORT = process.env.PORT || 3003;
-
-app.listen(PORT, function() {
-	console.log(`Server is running on port ${process.env.PORT || PORT}.`);
-});
